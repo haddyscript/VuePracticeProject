829b09fe7a8bb57f66fdef587f3fcafc06dc26d7
hadrianfdc <hadrianevarula@gmail.com>
2025-02-13 14:19:51 +0800

    Update Footer

--- app/frontend/src/components/FooterSection.vue
+++ app/frontend/src/components/FooterSection.vue
@@ -32,9 +32,8 @@
 
 				<div class="row g-5 mb-5">
 					<div class="col-lg-4">
-						<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo">Furni<span>.</span></a></div>
-						<p class="mb-4">Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant</p>
-
+						<div class="mb-4 footer-logo-wrap"><a href="#" class="footer-logo" v-if="displayBusinessName"> {{ displayBusinessName }}<span>.</span></a></div>
+						<p class="mb-4" v-if="business_detail">{{ business_detail.description }}</p>
 						<ul class="list-unstyled custom-social">
 							<li><a href="#"><span class="fa fa-brands fa-facebook-f"></span></a></li>
 							<li><a href="#"><span class="fa fa-brands fa-twitter"></span></a></li>
@@ -43,39 +42,36 @@
 						</ul>
 					</div>
 
-					<div class="col-lg-8">
+					<div class="col-lg-8" v-if="business_detail">
 						<div class="row links-wrap">
 							<div class="col-6 col-sm-6 col-md-3">
 								<ul class="list-unstyled">
-									<li><a href="#">About us</a></li>
-									<li><a href="#">Services</a></li>
-									<li><a href="#">Blog</a></li>
-									<li><a href="#">Contact us</a></li>
+									<li><router-link to="/about">About us</router-link></li>
+									<li><router-link to="/services">Services</router-link></li>
+									<li><router-link to="/blog">Blog</router-link></li>
+									<li><router-link to="/contact_us">Contact us</router-link></li>
 								</ul>
 							</div>
 
 							<div class="col-6 col-sm-6 col-md-3">
 								<ul class="list-unstyled">
-									<li><a href="#">Support</a></li>
-									<li><a href="#">Knowledge base</a></li>
-									<li><a href="#">Live chat</a></li>
+									<li><strong>Owner:</strong> {{ business_detail.owner_name }}</li>
+									<li><strong>Contact Person:</strong> {{ business_detail.contact_name }}</li>
+									<li><strong>Email:</strong> <a :href="'mailto:' + business_detail.email">{{ business_detail.email }}</a></li>	
 								</ul>
 							</div>
 
 							<div class="col-6 col-sm-6 col-md-3">
 								<ul class="list-unstyled">
-									<li><a href="#">Jobs</a></li>
-									<li><a href="#">Our team</a></li>
-									<li><a href="#">Leadership</a></li>
-									<li><a href="#">Privacy Policy</a></li>
+									<li><strong>Phone:</strong> <a :href="'tel:' + business_detail.phone_number">{{ business_detail.phone_number }}</a></li>
+									<li><strong>Founded:</strong> {{ formatFoundedDate }}</li>
 								</ul>
 							</div>
 
 							<div class="col-6 col-sm-6 col-md-3">
 								<ul class="list-unstyled">
-									<li><a href="#">Nordic Chair</a></li>
-									<li><a href="#">Kruzo Aero</a></li>
-									<li><a href="#">Ergonomic Chair</a></li>
+									<li><strong>Address:</strong> {{ business_detail.address }}, {{ business_detail.city }}, {{ business_detail.state }}, {{ business_detail.country }} - {{ business_detail.postal_code }}</li>
+									<li><strong>Business Hours:</strong> {{ business_detail.business_hours }}</li>
 								</ul>
 							</div>
 						</div>
@@ -87,10 +83,7 @@
 					<div class="row pt-4">
 						<div class="col-lg-6">
                             <p class="mb-2 text-center text-lg-start">
-                                Copyright &copy; {{ currentYear }}. All Rights Reserved. &mdash; Designed with love by
-                                <a href="https://untree.co" target="_blank" rel="noopener noreferrer">Untree.co</a>
-                                    Distributed By
-                                <a href="https://themewagon.com" target="_blank" rel="noopener noreferrer">ThemeWagon</a>
+                                Copyright &copy; {{ currentYear }}. All Rights Reserved.
                             </p>
 						</div>
 
@@ -109,7 +102,57 @@
 		<!-- End Footer Section -->	
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'; 
-const currentYear = ref(new Date().getFullYear());
+
+<script>
+import apiRequest from '@/services/apiService';
+
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      business_detail: null,
+	  currentYear: ''
+    };
+  },
+  computed: {
+    displayBusinessName() {
+        return this.business_detail?.name || "HadiStore";
+    },
+	formatFoundedDate() {
+      if (!this.business_detail || !this.business_detail.founded_year) return "N/A";
+      return this.formatDate(this.business_detail.founded_year);
+    }
+  },
+  watch : {
+	$route(to, from) {
+	    this.getBusinessDetail();
+    }
+  },
+  mounted() { 
+    this.getBusinessDetail();
+	const currentDate = new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+	this.currentYear = currentDate;
+  },
+  methods: {
+		formatDate(dateString) {
+			const date = new Date(dateString);
+			if (isNaN(date.getTime())) return "Invalid Date"; 
+			return date.toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" });
+		},
+		async getBusinessDetail(){
+            try{
+                const response = await apiRequest.getBusinessDetails();
+                if(response.data.success == "true"){
+                  this.business_detail = response.data.business;
+				  console.log(this.business_detail);
+                }else{
+                  this.business_detail = {};
+                }
+            }catch(error){
+                console.error('Error fetching business details:', error);
+                this.business_detail = {};
+            }
+        },
+	}
+}
 </script>
--- app/frontend/src/components/StartProductSection.vue
+++ app/frontend/src/components/StartProductSection.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import router from 'router';
+
+</script>
+
 <template>
     		<!-- Start Product Section -->
 		<div class="product-section">
@@ -14,7 +19,7 @@
 
 					<!-- Start Column 2 -->
 					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-						<a class="product-item" href="cart.html">
+						<router-link class="product-item" to="/shop">
 							<img src="/images/product-1.png" class="img-fluid product-thumbnail">
 							<h3 class="product-title">Nordic Chair</h3>
 							<strong class="product-price">$50.00</strong>
@@ -22,13 +27,13 @@
 							<span class="icon-cross">
 								<img src="/images/cross.svg" class="img-fluid">
 							</span>
-						</a>
+						</router-link>
 					</div> 
 					<!-- End Column 2 -->
 
 					<!-- Start Column 3 -->
 					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-						<a class="product-item" href="cart.html">
+						<router-link class="product-item" to="/shop">
 							<img src="/images/product-2.png" class="img-fluid product-thumbnail">
 							<h3 class="product-title">Kruzo Aero Chair</h3>
 							<strong class="product-price">$78.00</strong>
@@ -36,13 +41,13 @@
 							<span class="icon-cross">
 								<img src="/images/cross.svg" class="img-fluid">
 							</span>
-						</a>
+						</router-link>
 					</div>
 					<!-- End Column 3 -->
 
 					<!-- Start Column 4 -->
 					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-						<a class="product-item" href="cart.html">
+						<router-link class="product-item" to="/shop">
 							<img src="/images/product-3.png" class="img-fluid product-thumbnail">
 							<h3 class="product-title">Ergonomic Chair</h3>
 							<strong class="product-price">$43.00</strong>
@@ -50,7 +55,7 @@
 							<span class="icon-cross">
 								<img src="/images/cross.svg" class="img-fluid">
 							</span>
-						</a>
+						</router-link>
 					</div>
 					<!-- End Column 4 -->
 
